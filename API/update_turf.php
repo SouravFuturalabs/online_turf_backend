3683263e889d1503ef72bff5cc0bc0f9ccca990b
gana <ganamolrd@gmail.com>
2023-01-11 16:13:29 +0530

    11/1/23

--- API/update_turf.php
+++ API/update_turf.php
@@ -7,41 +7,41 @@ $Turf_location=$_POST["Turf_location"];
 $owner_acc=$_POST["owner_acc"];
 $owner_name=$_POST["owner_name"];
 //  $image=$_POST["image"];
-$pic=$_FILES['image']['name'];
-if($pic != "")
-{
-    $filearray=pathinfo($_FILES['image']['name']);
-    $file1=rand();
-   $file_ext=$filearray["extension"];
+// $pic=$_FILES['image']['name'];
+// if($pic != "")
+// {
+//     $filearray=pathinfo($_FILES['image']['name']);
+//     $file1=rand();
+//    $file_ext=$filearray["extension"];
 
-    $filenew=$file1 .".".$file_ext;
+//     $filenew=$file1 .".".$file_ext;
 
 
-    move_uploaded_file($_FILES['image']['tmp_name'],"../img/".$filenew);
+//     move_uploaded_file($_FILES['image']['tmp_name'],"../img/".$filenew);
 
-}
+// }
 
 $Owner_email=$_POST["Owner_email"];
 $Owner_emailold=$_POST["Owner_emailold"];
 $owner_ph=$_POST["owner_ph"];
 // $licence=$_POST["licence"];
-$lic=$_FILES['image']['name'];
-if($lic != "")
-{
-    $filearray=pathinfo($_FILES['image']['name']);
-    $file1=rand();
-   $file_ext=$filearray["extension"];
+// $lic=$_FILES['image']['name'];
+// if($lic != "")
+// {
+//     $filearray=pathinfo($_FILES['image']['name']);
+//     $file1=rand();
+//    $file_ext=$filearray["extension"];
 
-    $filenew1=$file1 .".".$file_ext;
+//     $filenew1=$file1 .".".$file_ext;
 
 
-    move_uploaded_file($_FILES['image']['tmp_name'],"../img/".$filenew1);
+//     move_uploaded_file($_FILES['image']['tmp_name'],"../img/".$filenew1);
 
-}
+// }
 $password=$_POST["password"];
 $rate=$_POST["rate"];
 
- $data=mysqli_query($con,"UPDATE `turf_tbl` SET `Turf_name`='$Turf_name',`Turf_location`='$Turf_location',`owner_acc`='$owner_name',`owner_name`='$owner_name',`image`='$filenew',`Owner_email`='$Owner_email',`owner_ph`='$owner_ph',`licence`='$filenew1',`rate`='$rate' WHERE Turf_id='$turf_id'");
+ $data=mysqli_query($con,"UPDATE `turf_tbl` SET `Turf_name`='$Turf_name',`Turf_location`='$Turf_location',`owner_acc`='$owner_name',`owner_name`='$owner_name',`Owner_email`='$Owner_email',`owner_ph`='$owner_ph',`rate`='$rate' WHERE Turf_id='$turf_id'");
  mysqli_query($con,"UPDATE `login_tbl` SET `uname`='$Owner_email',`password`='$password',`type`='turf' WHERE uname='$Owner_emailold'");
  if($data)
  {
